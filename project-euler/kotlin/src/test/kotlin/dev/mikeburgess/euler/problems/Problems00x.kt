a71476fd7fc87dfcee0f0fc3b7d51effbf19a8b6
Mike Burgess <mddburgess@users.noreply.github.com>
2022-03-12 20:15:18 -0800

    (Project Euler) Add problem 3.

--- project-euler/kotlin/src/test/kotlin/dev/mikeburgess/euler/problems/Problems00x.kt
+++ project-euler/kotlin/src/test/kotlin/dev/mikeburgess/euler/problems/Problems00x.kt
@@ -44,4 +44,25 @@ class Problems00x {
 
         assertThat(result).isEqualTo(4613732L)
     }
+
+    /**
+     * Problem 3
+     *
+     * The prime factors of 13195 are 5, 7, 13 and 29.
+     *
+     * What is the largest prime factor of the number 600851475143?
+     */
+    @Test
+    fun `Problem 3`() {
+        var number = 600_851_475_143
+        var factor = 2L
+        while (factor < number) {
+            when {
+                number % factor == 0L -> number /= factor
+                else -> factor++
+            }
+        }
+
+        assertThat(factor).isEqualTo(6857L)
+    }
 }
