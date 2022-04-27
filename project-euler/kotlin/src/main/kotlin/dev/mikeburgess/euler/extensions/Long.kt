dc1c965f273cce0ea5dd4d33323919a7f0746e28
Mike Burgess <mddburgess@users.noreply.github.com>
2022-04-26 21:40:01 -0700

    (Project Euler) Add problem 12.

--- project-euler/kotlin/src/main/kotlin/dev/mikeburgess/euler/extensions/Long.kt
+++ project-euler/kotlin/src/main/kotlin/dev/mikeburgess/euler/extensions/Long.kt
@@ -1,5 +1,27 @@
 package dev.mikeburgess.euler.extensions
 
+import dev.mikeburgess.euler.sequences.primeSequence
+import kotlin.math.sqrt
+
+fun Long.countDivisors() = when (this) {
+    1L -> 1
+    2L, 3L -> 2
+    else -> primeSequence()
+        .takeWhile { it <= sqrt(toDouble()) }
+        .map { countFactors(it) + 1 }
+        .reduce { a, b -> a * b }
+}
+
+fun Long.countFactors(n: Long): Int {
+    var count = 0
+    var temp = this
+    while (temp % n == 0L) {
+        temp /= n
+        count++
+    }
+    return count
+}
+
 fun Long.isEven() = this % 2 == 0L
 
 fun Long.isPalindrome(base: Long = 10L): Boolean {
--- project-euler/kotlin/src/main/kotlin/dev/mikeburgess/euler/sequences/Sequences.kt
+++ project-euler/kotlin/src/main/kotlin/dev/mikeburgess/euler/sequences/Sequences.kt
@@ -4,3 +4,8 @@ package dev.mikeburgess.euler.sequences
 fun fibonacciSequence() =
     generateSequence(1L to 2L) { (a, b) -> b to a + b }
         .map { it.first }
+
+@OEIS("A000217")
+fun triangleNumbers() =
+    generateSequence(1L to 2L) { (_, n) -> n * (n + 1) / 2 to n + 1 }
+        .map { it.first }
