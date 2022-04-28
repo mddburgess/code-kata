93c08d479f3c535acd9ecb951bfaf828604ade13
Mike Burgess <mddburgess@users.noreply.github.com>
2022-04-27 18:04:29 -0700

    (Project Euler) Add problem 19.

--- project-euler/kotlin/src/test/kotlin/dev/mikeburgess/euler/problems/Problems01x.kt
+++ project-euler/kotlin/src/test/kotlin/dev/mikeburgess/euler/problems/Problems01x.kt
@@ -12,6 +12,8 @@ import dev.mikeburgess.euler.sequences.primeSequence
 import dev.mikeburgess.euler.sequences.triangleNumbers
 import org.assertj.core.api.Assertions.assertThat
 import java.math.BigInteger
+import java.time.DayOfWeek
+import java.time.LocalDate
 import kotlin.test.Test
 
 class Problems01x {
@@ -339,5 +341,35 @@ class Problems01x {
         val result = TriangleGraph(getResourceAsInts("Problem018.txt")).maxSum()
 
         assertThat(result).isEqualTo(1074L)
+    }
+
+    /**
+     * Problem 19
+     *
+     * You are given the following information, but you may prefer to do some research for yourself.
+     *
+     * 1 Jan 1900 was a Monday.
+     *
+     * Thirty days has September,
+     * April, June and November.
+     * All the rest have thirty-one,
+     * Saving February alone,
+     * Which has twenty-eight, rain or shine.
+     * And on leap years, twenty-nine.
+     *
+     * A leap year occurs on any year evenly divisible by 4, but not on a century unless it is
+     * divisible by 400.
+     *
+     * How many Sundays fell on the first of the month during the twentieth century
+     * (1 Jan 1901 to 31 Dec 2000)?
+     */
+    @Test
+    fun `Problem 19`() {
+        val result = generateSequence(LocalDate.of(1901, 1, 1)) { it.plusMonths(1) }
+            .takeWhile { it <= LocalDate.of(2000, 12, 31) }
+            .count { it.dayOfWeek == DayOfWeek.SUNDAY }
+            .toLong()
+
+        assertThat(result).isEqualTo(171L)
     }
 }
